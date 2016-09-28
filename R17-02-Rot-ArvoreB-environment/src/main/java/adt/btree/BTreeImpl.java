54d705ff4b74b6a5e1478ea9f88ed342aa38ff21
Lucas Silva <lucas.silva@ccc.ufcg.edu.br>
2016-09-28 16:22:07 -0300

    setting up R18

--- R17-02-Rot-ArvoreB-environment/src/main/java/adt/btree/BTreeImpl.java
+++ R17-02-Rot-ArvoreB-environment/src/main/java/adt/btree/BTreeImpl.java
@@ -1,5 +1,8 @@
 package adt.btree;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class BTreeImpl<T extends Comparable<T>> implements BTree<T> {
 
 	protected BNode<T> root;
@@ -26,46 +29,88 @@ public class BTreeImpl<T extends Comparable<T>> implements BTree<T> {
 	}
 
 	private int height(BNode<T> node) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		if (node.isEmpty()) {
+			return -1;
+		} else {
+			return 1 + height(node.children.get(0));
+		}
 	}
 
 	@Override
 	public BNode<T>[] depthLeftOrder() {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		List<BNode<T>> list = new LinkedList<BNode<T>>();
+
+		addListDepthLeftOrder(root, list);
+
+		BNode<T>[] aux = new BNode[list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			aux[i] = list.get(i);
+		}
+		return aux;
+	}
+
+	private List<BNode<T>> addListDepthLeftOrder(BNode<T> node, List<BNode<T>> list) {
+		if (!node.isEmpty()) {
+			if (node.parent == null) {
+				list.add(node);
+			}
+
+			for (int i = 0; i < node.children.size(); i++) {
+				list.add(node.getChildren().get(i));
+
+				addListDepthLeftOrder(node.getChildren().get(i), list);
+			}
+		}
+		return list;
 	}
 
 	@Override
 	public int size() {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		return size(root);
+	}
+
+	public int size(BNode<T> node) {
+		if (node.isEmpty()) {
+			return 0;
+		} else {
+			int size = node.size();
+
+			for (BNode child : node.getChildren()) {
+				size += size(child);
+			}
+
+			return size;
+		}
 	}
 
 	@Override
 	public BNodePosition<T> search(T element) {
-		if (element != null){
+		if (element != null) {
 			return search(this.getRoot(), element);
 		} else {
 			return new BNodePosition<T>();
 		}
 	}
-	
-	private BNodePosition<T> search(BNode<T> node, T element){
-		int i = 0;
-		
-		while (i <= node.size() && element.compareTo(node.getElementAt(i)) > 0){
-			i++;
-		}
-		
-		if (i <= node.size() && element.equals(node.getElementAt(i))){
-			return new BNodePosition<T>(node, i);
-		}
-		
-		if (node.isLeaf()) {
+
+	private BNodePosition<T> search(BNode<T> node, T element) {
+		if (element == null || node == null) {
 			return new BNodePosition<T>();
 		}
 		
+		int i = 0;
+
+		while (i <= node.size() && element.compareTo(node.getElementAt(i)) > 0) {
+			i++;
+		}
+
+		if (i <= node.size() && element.equals(node.getElementAt(i))) {
+			return new BNodePosition<T>(node, i);
+		}
+
+		if (node.isLeaf()) {
+			return new BNodePosition<T>();
+		}
+
 		return search(node.getChildren().get(i), element);
 	}
 
@@ -79,6 +124,7 @@ public class BTreeImpl<T extends Comparable<T>> implements BTree<T> {
 	private void split(BNode<T> node) {
 		// TODO Implement your code here
 		throw new UnsupportedOperationException("Not Implemented yet!");
+
 	}
 
 	private void promote(BNode<T> node) {
